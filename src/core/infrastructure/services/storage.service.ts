3cb8748a494a7dfdf867aee082053b1bfc961318
Blankchen <b055254113@gmail.com>
2021-05-06 16:31:24 +0800

    Update storage.service.ts

--- src/core/infrastructure/services/storage.service.ts
+++ src/core/infrastructure/services/storage.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Organization } from 'src/core/domain/interfaces/organization';
 
 type LocalStorageKey = 'Organization';
 
@@ -6,26 +8,34 @@ type LocalStorageKey = 'Organization';
   providedIn: 'root'
 })
 export class StorageService {
+  organization = new BehaviorSubject<Organization>({} as Organization);
 
   constructor() { }
 
   set<Type>(key: LocalStorageKey, object: Type): void {
+    this.subjectHandler(key, object);
     const value = JSON.stringify(object);
     localStorage.setItem(key, value);
   }
 
   remove(key: LocalStorageKey): void {
     localStorage.removeItem(key);
-
   }
 
   get<Type>(key: LocalStorageKey): Type | null {
     const value = localStorage.getItem(key);
-    return value ? JSON.parse(value) : null;
+    const result = value ? JSON.parse(value) : null;
+    this.subjectHandler(key, result);
+    return result;
   }
 
   clear(): void {
     localStorage.clear();
+  }
 
+  private subjectHandler(key: LocalStorageKey, value: any): void {
+    if (key === 'Organization' && !!value) {
+      this.organization.next(value);
+    }
   }
 }
