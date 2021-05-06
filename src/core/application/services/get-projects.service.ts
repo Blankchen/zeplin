210553a052303886de23e027129852ef42446489
Blankchen <b055254113@gmail.com>
2021-05-06 16:31:07 +0800

    Update get-projects.service.ts

--- src/core/application/services/get-projects.service.ts
+++ src/core/application/services/get-projects.service.ts
@@ -5,6 +5,7 @@ import { Project } from 'src/core/domain/interfaces/project';
 import { ApiService } from 'src/core/infrastructure/services/api.service';
 import { StorageService } from 'src/core/infrastructure/services/storage.service';
 import { ApplicationService } from '../interfaces/application-service';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,10 +22,9 @@ export class GetProjectsService implements ApplicationService<void, Project[]> {
   }
 
   private getProjects(): Observable<Project[]> {
-    const organization = this.storageService.get<Organization>('Organization');
-    if (!organization) {
-      return of([] as Project[]);
-    }
-    return this.apiService.getProjects(organization.id);
+    return this.storageService.organization.pipe(
+      filter(x => !!x.id),
+      switchMap(organization => this.apiService.getProjects(organization.id))
+    );
   }
 }
